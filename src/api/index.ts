6c13d84caf0b2d99efaddf087fef07e939c7110d
ChenGengyang <1085684645@qq.com>
2020-02-24 22:54:04 +0800

    feat: 添加搜索接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,27 @@
-export default {};
+import axios from "@/plugins/axios";
+import {
+  AdvancedSearchData,
+  BasicSearchData
+} from "@/interfaces/components/search/SearchData";
+import { BasicResponse } from "@/interfaces/responses/BasicResponse";
+import { SearchResponse } from "@/interfaces/responses/search/SearchResponse";
+
+// 1. 普通搜索
+export async function basicSearch(
+  args: BasicSearchData
+): Promise<BasicResponse<SearchResponse[]>> {
+  const { data } = await axios.get("/search/basic", {
+    params: { ...args }
+  });
+  return data;
+}
+
+// 2. 组合搜索
+export async function advancedSearch(
+  args: AdvancedSearchData
+): Promise<BasicResponse<SearchResponse[]>> {
+  const { data } = await axios.get("/search/advanced", {
+    params: { ...args }
+  });
+  return data;
+}
