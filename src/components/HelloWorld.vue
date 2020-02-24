ca920d3f7456110508d03dba508f1b5544281bd6
ChenGengyang <1085684645@qq.com>
2020-02-24 22:27:02 +0800

    chore: 构建全局stylesheets

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -131,8 +131,10 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+@import "../../src/stylesheets/default.less";
 h3 {
   margin: 40px 0 0;
+  background: @background-blue;
 }
 ul {
   list-style-type: none;
