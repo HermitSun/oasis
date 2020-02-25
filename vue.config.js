50d0eaa2ebb29eaa618d193b6cebb464ecc0cb21
Wen Sun <syl1887415157@126.com>
2020-02-26 00:13:59 +0800

    fix(ci): 只在非生产环境启动json-server

--- vue.config.js
+++ vue.config.js
@@ -46,16 +46,19 @@ module.exports = {
   }
 };
 
-/*----------------json-server---------*/
-const jsonServer = require("json-server");
-/*搭建一个server*/
-const apiServer = jsonServer.create();
-/*将db.json关联到server*/
-const apiRouter = jsonServer.router("src/api/json-server/ResponseJson.json");
-const middleWares = jsonServer.defaults();
-apiServer.use(middleWares);
-apiServer.use(apiRouter);
-/*监听端口*/
-apiServer.listen(3180, () => {
-  console.log("JSON Server is running");
-});
+// 非生产环境启动
+if (process.env.NODE_ENV !== "production") {
+  /*----------------json-server---------*/
+  const jsonServer = require("json-server");
+  /*搭建一个server*/
+  const apiServer = jsonServer.create();
+  /*将db.json关联到server*/
+  const apiRouter = jsonServer.router("src/api/json-server/ResponseJson.json");
+  const middleWares = jsonServer.defaults();
+  apiServer.use(middleWares);
+  apiServer.use(apiRouter);
+  /*监听端口*/
+  apiServer.listen(3180, () => {
+    console.log("JSON Server is running");
+  });
+}
