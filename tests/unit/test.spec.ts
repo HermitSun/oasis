e4c66bfe9a63deea1550c69b09cae06ae7c03ccd
Wen Sun <syl1887415157@126.com>
2020-03-01 21:37:36 +0800

    test: 增加基础测试

---
+++ tests/unit/test.spec.ts
@@ -0,0 +1,45 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import VueRouter from "vue-router";
+import App from "@/App.vue";
+
+import { advancedSearch, basicSearch } from "@/api";
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
+describe("App.vue", () => {
+  it("renders correctly", () => {
+    const wrapper = shallowMount(App, { localVue });
+    expect(wrapper.find("#app").isVisible()).toBe(true);
+  });
+
+  it("basic search succeed", async () => {
+    try {
+      const searchRes = await basicSearch({ keyword: "keyword", page: 1 });
+      expect(searchRes).not.toBeNull();
+      expect(searchRes).not.toBeUndefined();
+      expect(searchRes.data.length).not.toBe(0);
+    } catch (e) {
+      // network error
+      expect(e.toString()).toContain("undefined");
+    }
+  });
+
+  it("advanced search succeed", async () => {
+    try {
+      const searchRes = await advancedSearch({
+        author: "123", // 作者
+        affiliation: "123", // 机构
+        conferenceName: "123", // 会议
+        keyword: "123", // 研究关键字
+        page: 1
+      });
+      expect(searchRes).not.toBeNull();
+      expect(searchRes).not.toBeUndefined();
+      expect(searchRes.data.length).toBe(0);
+    } catch (e) {
+      // network error
+      expect(e.toString()).toContain("undefined");
+    }
+  });
+});
