b04b17aee310c9348b0ddb47aa9a5f3fa8385b88
Wen Sun <syl1887415157@126.com>
2020-03-02 00:04:51 +0800

    chore(jest): 调整测试覆盖报告范围

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript",
   collectCoverage: true,
-  collectCoverageFrom: ["**/{!(*.d),}.{ts,vue}", "!**/node_modules/**"]
+  collectCoverageFrom: [
+    "src/{!(router|store),}/{!(*.d),}.{ts,vue}",
+    "!**/node_modules/**"
+  ]
 };
