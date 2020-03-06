c63c5ae3aa9bdad1fd42ed2810f3e122b343b54a
Wen Sun <syl1887415157@126.com>
2020-03-06 10:10:36 +0800

    fix(json-server): 修复json-server的错误路径

--- json-server.js
+++ json-server.js
@@ -1,13 +1,19 @@
+const path = require("path");
+
+function resolve(filename) {
+  return path.join(__dirname, filename);
+}
+
 /*----------------json-server---------*/
 const jsonServer = require("json-server");
-/*搭建一个server*/
 const apiServer = jsonServer.create();
-/*将db.json关联到server*/
-const apiRouter = jsonServer.router("src/api/json-server/ResponseJson.json");
+const apiRouter = jsonServer.router(
+  resolve("./src/api/json-server/ResponseJson.json")
+);
 const middleWares = jsonServer.defaults();
 apiServer.use(middleWares);
 apiServer.use(apiRouter);
-/*监听端口*/
+
 apiServer.listen(3180, () => {
   console.log("JSON Server is running");
 });
