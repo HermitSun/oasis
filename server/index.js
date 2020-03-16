fd06e069b38e432517e5870fb20d9d333453f091
Alexchanchic <1085684645@qq.com>
2020-03-16 16:38:07 +0800

    test(express): 测试方法替换为express

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
+import basicSearchMockData from '../assets/mock/basicSearchMockData';
+
 const app = express();
 
 app.get('/echo/:what', (req, res) => {
   res.json(req.params);
 });
 
+app.get('/search/basic/mongo', (req, res) => {
+  console.log(req.params);
+  res.json(basicSearchMockData);
+});
+
 // const charts = require('./routes/charts');
 // app.use(charts);
 
 module.exports = {
-  path: '/api',
+  path: '',
   handler: app
 };
