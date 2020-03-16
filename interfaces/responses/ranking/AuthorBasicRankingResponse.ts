e17e0a6b307261620dc8acabf02a5e2b5fa80733
Alexchanchic <1085684645@qq.com>
2020-03-16 15:48:28 +0800

    fix(interface): 按照接口文档，修改researchId为authorId

--- interfaces/responses/ranking/AuthorBasicRankingResponse.ts
+++ interfaces/responses/ranking/AuthorBasicRankingResponse.ts
@@ -1,11 +1,11 @@
 /**
  * 搜索返回值：学者论文简略排名
  * @author Alexchanchic
- * @date 2020.03.03
+ * @date 2020.03.16
  */
 
 export interface AuthorBasicRankingResponse {
   name: string;
   count: number;
-  researcherId: string;
+  authorId: string;
 }
