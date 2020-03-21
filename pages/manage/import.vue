8ead57031877e7ccd1dee128bc4c820c8c74a5fe
Wen Sun <syl1887415157@126.com>
2020-03-21 23:34:21 +0800

    refactor: 对管理员端进行迁移

---
+++ pages/manage/import.vue
@@ -0,0 +1,103 @@
+<template>
+  <div style="text-align: center">
+    <h2>论文数据导入</h2>
+    <el-upload
+      ref="paperUploader"
+      action=""
+      :limit="1"
+      :auto-upload="false"
+      :on-remove="removePaper"
+      :on-change="handleFileChange"
+      :on-exceed="handleFileExceed"
+    >
+      <template #trigger>
+        <el-button
+          :type="!isUploadValid ? 'primary' : 'default'"
+          :disabled="isUploadValid"
+          class="upload-selector"
+        >
+          选取本地文件
+        </el-button>
+      </template>
+      <el-button
+        :type="isUploadValid ? 'primary' : 'default'"
+        style="margin-left: 10px;"
+        :disabled="!isUploadValid"
+        class="upload-uploader"
+        @click="doUpload"
+      >
+        导入选定文件
+      </el-button>
+      <template #tip>
+        <div class="el-upload__tip">
+          请上传<b>1个</b>不超过<b>10MB</b>的<b>JSON / CSV</b>文件
+        </div>
+      </template>
+    </el-upload>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { ElUpload, ElUploadInternalFileDetail } from 'element-ui/types/upload';
+import { importPaperData } from '~/api';
+
+export default Vue.extend({
+  name: 'ManageImport',
+  data() {
+    return {
+      papers: process.client ? new FormData() : undefined,
+      isUploadValid: false
+    };
+  },
+  methods: {
+    // 移除paper
+    removePaper(file: { status: string; raw: File }, fileList: File[]) {
+      console.log(file, fileList);
+      this.papers!.delete('paperData');
+      this.isUploadValid = false;
+    },
+    // 判断文件是否符合标准
+    isProperJSONOrCSV(file: File) {
+      const isJSON =
+        file.type === 'application/json' || file.name.endsWith('.json');
+      const isCSV =
+        file.type === 'application/vnd.ms-excel' || file.name.endsWith('.csv');
+      const isUnderLimit = file.size / 1024 / 1024 <= 10;
+      return (isJSON || isCSV) && isUnderLimit;
+    },
+    // 文件修改时调用，主要是用来上传
+    handleFileChange(file: ElUploadInternalFileDetail, fileList: File[]) {
+      console.log(file, fileList);
+      if (file.status === 'ready') {
+        if (this.isProperJSONOrCSV(file.raw)) {
+          this.papers!.set('paperData', file.raw);
+        } else {
+          this.$message.error('请按要求上传文件');
+          fileList.splice(fileList.length - 1, 1);
+        }
+      }
+      this.isUploadValid = fileList.length !== 0;
+    },
+    handleFileExceed() {
+      this.$message.error('文件数量超过限制');
+    },
+    // 进行上传
+    async doUpload() {
+      console.log(this.papers!.get('paperData'));
+      const paperUploader = this.$refs.paperUploader as ElUpload;
+      paperUploader.submit();
+      const importRes = await importPaperData(this.papers as FormData);
+      if (importRes && importRes.code === 200) {
+        this.$message.success(
+          `成功导入了${importRes.data.increasedCount}条数据`
+        );
+        // clear
+        this.papers = new FormData();
+      } else {
+        this.$message.error('导入失败');
+      }
+    }
+  }
+});
+</script>
