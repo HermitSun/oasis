43c37324e7b05543996103db8d80c5d8953c9588
Wen Sun <syl1887415157@126.com>
2020-04-04 13:05:32 +0800

    feat(papers): 增加更新论文信息接口

--- api/index.ts
+++ api/index.ts
@@ -181,7 +181,7 @@ const mockConfig = {
   baseURL:
     process.env.NODE_ENV === 'production'
       ? 'https://wensun.top'
-      : 'http://localhost:3000/test',
+      : 'http://101.37.175.237:8081/',
   timeout: 60 * 1000
 };
 const mockAxios = globalAxios.create(mockConfig);
@@ -252,8 +252,7 @@ export async function updateJournalInfo(
 export async function updatePaperInfo(
   args: UpdatePaperInfoPayload
 ): Promise<BasicResponse> {
-  // TODO: 切换成真实的URL
-  const { data } = await mockAxios.put('/papers/update', args);
+  const { data } = await axios.put('/info/paper', args);
   return data;
 }
 
