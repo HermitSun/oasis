518127d457f7391503c8481d304e10b932d1f44c
Wen Sun <syl1887415157@126.com>
2020-04-04 20:18:30 +0800

    fix(wordcloud): 修复词云在SSR环境下控制台报错的问题

--- components/interest/InterestWordCloud.vue
+++ components/interest/InterestWordCloud.vue
@@ -20,10 +20,14 @@
 
 <script lang="ts">
 import Vue from 'vue';
+
 export default Vue.extend({
   name: 'InterestWordCloud',
+  // 此处的实现确实不是很必要，但是为了控制台不报错，加了个没什么用的判断
   components: {
-    wordcloud: () => import('vue-wordcloud')
+    wordcloud: process.client
+      ? () => import('vue-wordcloud')
+      : () => import('~/components/AsyncCompLoading')
   },
   props: {
     interests: {
