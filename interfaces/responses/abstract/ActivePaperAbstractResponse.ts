e761cdc1705c3ba5c7844b7193bda8006cd0aeb4
Wen Sun <syl1887415157@126.com>
2020-04-08 23:14:35 +0800

    refactor(index): 首页活跃论文作者可跳转到画像

--- interfaces/responses/abstract/ActivePaperAbstractResponse.ts
+++ interfaces/responses/abstract/ActivePaperAbstractResponse.ts
@@ -2,12 +2,17 @@
  * 活跃论文的返回值
  * @author WenSun
  * @date 2020.03.06
- * @update 2020.03.15
+ * @update 2020.04.08
  */
+
+export interface AuthorInfo {
+  name: string;
+  id: string;
+}
 
 export interface ActivePaperAbstractResponse {
   title: string;
-  authors: string[];
+  authors: AuthorInfo[];
   _abstract: string;
   link: string;
   publicationYear: number;
