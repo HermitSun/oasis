71c6b398460a586c63654e3938207b705bcf86ab
Wen Sun <syl1887415157@126.com>
2020-06-29 09:46:02 +0800

    chore(api): 调整后端部署位置

--- api/config.ts
+++ api/config.ts
@@ -4,11 +4,7 @@ const globalConfig = {
   baseURL:
     process.env.NODE_ENV === 'production'
       ? 'https://wensun.top/api'
-      : 'http://34.102.235.205/prod', // 测试
-  // : 'http://34.102.235.205/test', // 正式
-  // : 'http://localhost:3000/test',
-  // : 'http://47.101.33.219:8081',
-
+      : 'https://wensun.top/api', // 测试
   timeout: 60 * 1000
   // 如果启用凭据，后端必须在AllowOrigin里指定源，而不能是*
   // withCredentials: true
