bddb3ffd3e3e1deb8ab5e971f068a410362c6be6
Wen Sun <syl1887415157@126.com>
2020-07-27 10:07:41 +0800

    fix(ehcarts): 修复CDN导入echarts顺序错误的问题

--- components/mixins/AsyncLoadEcharts.ts
+++ components/mixins/AsyncLoadEcharts.ts
@@ -1,37 +1,31 @@
 import Vue from 'vue';
 
 const load = Vue.extend({
-  data() {
-    return {
-      hasEchartsLoaded: 0
-    };
-  },
-  watch: {
-    hasEchartsLoaded(val) {
-      if (val === 2) {
-        this.onEchartsLoad();
-      }
+  mounted() {
+    // if not exists, load
+    if (window.echarts && window.echarts.graphicGL) {
+      console.log('echarts has loaded');
+      this.onEchartsLoad();
+    } else {
+      // 必须串行，echarts-gl似乎是依赖于echarts的
+      const echarts = document.createElement('script');
+      echarts.src =
+        'https://cdn.jsdelivr.net/npm/echarts@4.8.0/dist/echarts.min.js';
+      echarts.async = true;
+      // loaded
+      echarts.addEventListener('load', () => {
+        const echartsGL = document.createElement('script');
+        echartsGL.src =
+          'https://cdn.jsdelivr.net/npm/echarts-gl@1.1.1/dist/echarts-gl.min.js';
+        echartsGL.async = true;
+        echartsGL.addEventListener('load', () => {
+          this.onEchartsLoad();
+        });
+        document.head.appendChild(echartsGL);
+      });
+      // apply to html
+      document.head.appendChild(echarts);
     }
-  },
-  beforeMount() {
-    // load
-    // 必须串行，echarts-gl似乎是依赖于echarts的
-    const echarts = document.createElement('script');
-    echarts.src =
-      'https://cdn.jsdelivr.net/npm/echarts@4.8.0/dist/echarts.min.js';
-    const echartsGL = document.createElement('script');
-    echartsGL.src =
-      'https://cdn.jsdelivr.net/npm/echarts-gl@1.1.1/dist/echarts-gl.min.js';
-    // loaded
-    echarts.addEventListener('load', () => {
-      ++this.hasEchartsLoaded;
-    });
-    echartsGL.addEventListener('load', () => {
-      ++this.hasEchartsLoaded;
-    });
-    // apply to html
-    document.head.appendChild(echarts);
-    document.head.appendChild(echartsGL);
   },
   methods: {
     // placeholder
